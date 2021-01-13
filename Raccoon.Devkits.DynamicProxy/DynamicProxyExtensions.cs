e9ef393eb5a8ecbbf12b7bcdaf6d841992de38a5
lhrbuxiaoxin <lihaoran228@163.com>
2021-01-13 20:28:41 +0800

    添加项目文件。

---
+++ Raccoon.Devkits.DynamicProxy/DynamicProxyExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raccoon.Devkits.DynamicProxy
+{
+    public static class DynamicProxyExtensions
+    {
+        public static IServiceCollection AddTransientProxy<TIService, TImplementation, TProxy>(this IServiceCollection services) 
+            where TIService : class 
+            where TImplementation : class, TIService 
+            where TProxy : DynamiceProxyBase<TIService> =>
+            services.AddTransient<TImplementation>()
+                .AddTransient(RegisterServiceProxy<TIService, TImplementation, TProxy>);
+        public static IServiceCollection AddScopedProxy<TIService, TImplementation, TProxy>(this IServiceCollection services)
+           where TIService : class
+           where TImplementation : class, TIService
+           where TProxy : DynamiceProxyBase<TIService> =>
+           services.AddScoped<TImplementation>()
+               .AddScoped(RegisterServiceProxy<TIService, TImplementation, TProxy>);
+
+        public static IServiceCollection AddSingletonProxy<TIService, TImplementation, TProxy>(this IServiceCollection services)
+            where TIService : class
+            where TImplementation : class, TIService
+            where TProxy : DynamiceProxyBase<TIService> =>
+            services.AddSingleton<TImplementation>()
+                .AddSingleton(RegisterServiceProxy<TIService, TImplementation, TProxy>);
+
+        private static TIService RegisterServiceProxy<TIService, TImplementation, TProxy>(IServiceProvider serviceProvider)
+            where TIService : class 
+            where TImplementation : class, TIService 
+            where TProxy : DynamiceProxyBase<TIService>
+        {
+            TProxy proxy = (DispatchProxy.Create<TIService, TProxy>() as TProxy)!;
+            TImplementation implementation = serviceProvider.GetRequiredService<TImplementation>();
+            proxy.Target = implementation;
+            proxy.ServiceProvider = serviceProvider;
+            return (proxy as TIService)!;
+        }
+    }
+}
---
+++ Raccoon.Devkits.DynamicProxy/DynamiceProxyBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raccoon.Devkits.DynamicProxy
+{
+    public abstract class DynamiceProxyBase<TIService> : DispatchProxy where TIService : class
+    {
+        public TIService Target { get; set; } = null!;
+        public IServiceProvider ServiceProvider { get; set; } = null!;
+    }
+}
